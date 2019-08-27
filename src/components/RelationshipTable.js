8a3d0cd07870ad85e416bdbb7d6f35f6230ca065
thupx <xuanthu404@gmail.com>
2019-08-27 13:42:40 +0700

    fix : show table

--- src/components/RelationshipTable.js
+++ src/components/RelationshipTable.js
@@ -7,7 +7,8 @@ class RelationshipTable extends React.Component {
         this.state = {
             items: [],
             data: [],
-        }
+        };
+        // this.renderEditable = this.renderEditable.bind(this);
     }
     componentDidMount() {
         this.computeItems();
@@ -31,7 +32,7 @@ class RelationshipTable extends React.Component {
             const nodeId = doc.id.split('_NODE')[0];
             console.log('nodeids : ', nodeId);
             for (let i = 0; i < storeTitle.length; i++) {
-                if (storeTitle[i].id.includes(nodeId)) {
+                if (storeTitle[i].id.includes('_CODE') && storeTitle[i].id.includes(nodeId)) {
                     return storeTitle[i].firstChild.textContent;
                 }
             }
@@ -41,20 +42,14 @@ class RelationshipTable extends React.Component {
                 const type = getType(node);
                 const neighbor = {
                     id: node,
-                    name: type === 'path' ? 'path' : getName(node),
-                    type: type
+                    name: type === 'path' ? node : getName(node),
+                    type: type,
+                    cost: object[node]
                 }
                 return neighbor;
             })
         }
         const graphs = {
-            "youarehere_NODE": {
-                "L4_PATH_70_NODE": 27,
-                "L4_PATH_43_NODE": 27,
-                "L4_PATH_10_NODE": 71,
-                "L4_PATH_45_NODE": 53,
-                "L4_24_NODE": 66
-            },
             "L4_PATH_70_NODE": {
                 "youarehere_NODE": 27,
                 "L4_PATH_69_NODE": 22,
@@ -79,6 +74,10 @@ class RelationshipTable extends React.Component {
             "L4_21B_NODE": {
                 "L4_PATH_48_NODE": 16
             },
+            "L4_23_NODE": {
+                "L4_PATH_52_NODE": 19,
+                "L4_PATH_59_NODE": 22
+            },
         }
         const graphsArray = Object.keys(graphs).map(node => {
             const type = getType(node);
@@ -86,14 +85,15 @@ class RelationshipTable extends React.Component {
                 node: {
                     id: node,
                     type: type,
-                    name: type === 'path' ? 'path' : getName(node)
+                    name: type === 'path' ? node : getName(node)
                 },
                 neighbors: transformNeighbors(graphs[node])
             }
             return relation;
         });
-        // console.log('graphs', graphsArray);
-        return graphsArray;
+        console.log('graphs', graphsArray);
+        // return graphsArray;
+        this.setState({ data: graphsArray });
     }
     render() {
         const styles = {
@@ -102,110 +102,128 @@ class RelationshipTable extends React.Component {
         }
         const nameItems = [];
         const typeItems = [];
-        // const { nodes } = this.props;
-        const nodes = this.computeItems()
+        const nodes = this.state.data
         for (let i = 0; i < nodes.length; i++) {
-            nameItems.push(<option key={i} value={nodes[i].node.id}>
-                {nodes[i].node.name}
-            </option>);
-            typeItems.push(<option key={i} value={nodes[i].type}>
-                {nodes[i].node.type}
-            </option>)
+            if (!nameItems.includes(nodes[i].node.name))
+                nameItems.push(<option key={i} value={nodes[i].node.id}>
+                    {nodes[i].node.name}
+                </option>);
+            if (!typeItems.includes(nodes[i].node.type))
+                typeItems.push(<option key={i} value={nodes[i].type}>
+                    {nodes[i].node.type}
+                </option>)
         }
-        const data = this.computeItems();
+        const changeProperty = (object, property) => {
+            return <div
+                style={{ backgroundColor: "#fafafa" }}
+                contentEditable
+                suppressContentEditableWarning
+                onBlur={e => {
+                    const { data } = this.state;
+                    if (object[`${property}`] !== e.target.innerHTML) {
+                        object[`${property}`] = e.target.innerHTML;
+                        this.setState({ data });
+                    }
+                    this.setState({ data });
+                }}
+                dangerouslySetInnerHTML={{
+                    __html: object[`${property}`]
+                }}
+            />
+        }
         const columns = [{
             Header: 'Node',
-            Cell: row => (
-                <div>
-                    <select style={{
-                        width: '100%',
-                        height: '100%',
-                        backgroundColor: '#dadada',
-                        borderRadius: '3px',
-                    }}>
-                        {nameItems}
-                    </select>
-                </div>
-            )
+            Cell: props => {
+                const { node } = props.original;
+                return <div
+                    // style={{ backgroundColor: "#fafafa" }}
+                    // contentEditable={false}
+                    // suppressContentEditableWarning
+                    dangerouslySetInnerHTML={{
+                        __html: node.name
+                    }}
+                />
+            }
         }, {
             Header: 'Type',
             accessor: 'node.type',
             width: 200,
-            Cell: props => (
-                <div>
-                    <select style={{
-                        width: '100%',
-                        height: '100%',
-                        backgroundColor: '#dadada',
-                        borderRadius: '3px',
-                    }}>
-                        {typeItems}
-                    </select>
-                </div>
-            ) // Custom cell components!
+            Cell: props => {
+                const { node } = props.original;
+                return <div
+                    style={{ backgroundColor: "#fafafa", justifyContent: "center" }}
+                    contentEditable={false}
+                    suppressContentEditableWarning
+                    dangerouslySetInnerHTML={{
+                        __html: node.type
+                    }}
+                />
+            }
         }, {
-            id: 'neighbors', // Required because our accessor is not a string
+            id: 'neighbors',
             Header: 'Neighbors',
-            accessor: d => d.neighbors.map(neighbor => neighbor.name),//d.friend.name // Custom value accessors!
-            Cell: props => (
-                <div>
-                    <select style={{
-                        width: '100%',
-                        height: '100%',
-                        backgroundColor: '#dadada',
-                        borderRadius: '3px',
-                    }}>
-                        {nameItems}
-                    </select>
-                </div>
-            )
+            accessor: d => d.neighbors.map(neighbor => neighbor.name),
+            Cell: props => {
+                const { neighbors } = props.original;
+                return neighbors.map(neighbor => {
+                    return changeProperty(neighbor, 'name')
+                })
+            }
         }, {
-            Header: props => <span>Type</span>, // Custom header components!
-            // accessor: 'friend.age'
-            accessor: 'neighbors.type',
-            Cell: props => (
-                <div>
-                    <select style={{
-                        width: '100%',
-                        height: '100%',
-                        backgroundColor: '#dadada',
-                        borderRadius: '3px',
-                    }}>
-                        {typeItems}
-                    </select>
-                </div>
-            ),
+            Header: props => <span>Type</span>,
+            // accessor: 'neighbors.type',
+            Cell: props => {
+                const { neighbors } = props.original;
+                return neighbors.map(neighbor => {
+                    return changeProperty(neighbor, 'type')
+                })
+            },
             width: 200,
         },
         {
             id: 'cost',
             Header: 'Cost',
             accessor: d => 58,
-            width: 100
+            width: 100,
+            Cell: props => {
+                const { neighbors } = props.original;
+                return neighbors.map(neighbor => {
+                    return <div
+                        style={{ backgroundColor: "#fafafa" }}
+                        contentEditable={false}
+                        suppressContentEditableWarning
+                        dangerouslySetInnerHTML={{
+                            __html: neighbor.cost
+                        }}
+                    />
+                })
+            }
         },
         {
             Header: 'Action',
-            Cell: row => (
-                <div>
-                    <button style={styles} onClick={() => alert('Save')}>Save</button>
-                    <button style={styles} onClick={() => alert('Delete')}>Delete</button>
-                </div>
-            ),
+            Cell: props => {
+                const { neighbors } = props.original;
+                return neighbors.map(neighbor => {
+                    return <div>
+                        <button style={styles} onClick={() => alert('Removed neighbor ' + neighbor.name)}>Remove</button>
+                    </div>
+                })
+            },
             width: 100
         }
         ]
         return <div>
-            <ReactTable
-                data={data}
-                columns={columns}
-                defaultPageSize={2}
-                showPagination={false}
-            />
             <div style={{
                 textAlign: 'right',
             }}>
                 <button style={styles} >Add</button>
             </div>
+            <ReactTable
+                data={nodes}
+                columns={columns}
+                defaultPageSize={10}
+                showPagination={true}
+            />
         </div>
 
     }
