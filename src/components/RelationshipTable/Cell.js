e195da5ca7e9626739707205b4137d4bb0441f7d
voquochuyhuy <37115874+voquochuyhuy@users.noreply.github.com>
2019-09-16 19:36:51 +0700

    highlight vertex

--- src/components/RelationshipTable/Cell.js
+++ src/components/RelationshipTable/Cell.js
@@ -1,5 +1,46 @@
 import React from 'react';
+import { timeout } from 'q';
+// import {attr,animate} from "svg.js";
 export const Cell = ({ node, neighbor, property, propertyToEdit, canEdit, onBlur }) => {
+    const highLightEl = ()=>{
+        
+        if(node !== undefined)
+        {
+            let svgEl = document.getElementById(node.id);
+            
+            var anim = document.createElementNS("http://www.w3.org/2000/svg", "animate")
+            anim.setAttribute("begin", "indefinite")
+            anim.setAttribute("from", 10)
+            anim.setAttribute("to", 50)
+            anim.setAttribute("fill", "freeze")
+            anim.setAttribute("dur", "1s")
+            anim.setAttribute("repeatCount", "indefinite")
+            anim.setAttribute("attributeName", "r")
+
+            svgEl.appendChild(anim);
+            anim.beginElement();
+            timeout(()=>{anim.parentElement.removeChild(anim);console.log("object");},3000);
+          
+        }
+        if(neighbor !== undefined)
+        {
+            let svgEl = document.getElementById(neighbor.id);
+            var anim = document.createElementNS("http://www.w3.org/2000/svg", "animate")
+            anim.setAttribute("begin", "indefinite")
+            anim.setAttribute("from", 10)
+            anim.setAttribute("to", 50)
+            anim.setAttribute("fill", "freeze")
+            anim.setAttribute("dur", "1s")
+            anim.setAttribute("repeatCount", "indefinite")
+            anim.setAttribute("attributeName", "r")
+
+            svgEl.appendChild(anim);
+            anim.beginElement();
+            setTimeout(function() { anim.parentElement.removeChild(anim); }, 2000);
+        
+        }
+        
+    }
     if (typeof canEdit !== "boolean") canEdit = false;
     return (
         <div
@@ -10,6 +51,7 @@ export const Cell = ({ node, neighbor, property, propertyToEdit, canEdit, onBlur
             dangerouslySetInnerHTML={{
                 __html: canEdit ? neighbor[propertyToEdit] : node[property]
             }}
+            onClick= {()=>{highLightEl(node,neighbor)} }
         />
     )
 }
